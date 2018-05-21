0e058681c6996784ddccb9d7c933f8bd75e8462d
anthony952 <anthony@codineffable.com>
2018-05-21 17:46:39 -0400

    v1
    a

---
+++ js/codineffable.js
@@ -0,0 +1,20 @@
+$( document ).ready(function() {
+
+
+
+console.log(name);
+$('#submit-form').click(function(event){
+  var numero=$("#name").val();
+  var mensaje=$("#description").val();
+
+  event.preventDefault();
+  console.log(numero+mensaje)
+  window.location.href='https://web.whatsapp.com/send?phone='+numero+'&text='+mensaje+'&source=&data=';
+})
+
+$('.logoFooter').on('click',function(event){
+    window.location.href='https://codineffable.github.io/';
+
+});
+});
+
